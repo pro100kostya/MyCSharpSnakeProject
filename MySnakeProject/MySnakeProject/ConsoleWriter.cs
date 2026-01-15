624e71cb4eb1eb5b81e84405a3f22cbdd048b462
pro100kostya <154823121+pro100kostya@users.noreply.github.com>
2026-01-15 17:36:10 +0300

    Movement update
    Управление перенесено на WASD

---
+++ MySnakeProject/MySnakeProject/ConsoleWriter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySnakeProject
+{
+    internal static class ConsoleWriter
+    {
+        public static void WriteCurrentUnitPosition(Unit unit)
+        {
+            Console.WriteLine($"X = {unit.CurrentPosition.X} , Y = {unit.CurrentPosition.Y}");
+        }
+    }
+}
---
+++ MySnakeProject/MySnakeProject/Unit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySnakeProject
+{
+    internal abstract class Unit
+    {
+        public Vector2 CurrentPosition;
+
+        public abstract void Update();
+    }
+}
