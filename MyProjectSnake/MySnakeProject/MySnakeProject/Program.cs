26ce102eb2e96cc305f3a2f32848e36df43bbd5d
pro100kostya <154823121+pro100kostya@users.noreply.github.com>
2026-01-15 17:41:49 +0300

    movement update
    Now controls are WASD

---
+++ MyProjectSnake/MySnakeProject/MySnakeProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using MySnakeProject;
+
+internal class Program
+{
+    public static void Main()
+    {
+        ConsoleInput consoleInput = new ConsoleInput();
+        PlaybleUnitSnake player = new(new Vector2(0, 0), consoleInput);
+
+        while (true)
+        {
+            consoleInput.Update();
+            player.Update();
+
+            Thread.Sleep(1000);  
+        }
+    }
+}
