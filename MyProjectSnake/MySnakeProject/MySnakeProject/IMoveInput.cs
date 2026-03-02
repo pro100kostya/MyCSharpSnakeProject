2db85f21fb69555c188f95db399973d95abb4615
pro100kostya <154823121+pro100kostya@users.noreply.github.com>
2026-03-02 18:40:08 +0300

    Add files via upload

---
+++ MyProjectSnake/MySnakeProject/MySnakeProject/IMoveInput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySnakeProject
+{
+    internal interface IMoveInput
+    {
+        public event Action MoveUp;
+        public event Action MoveDown;
+        public event Action MoveLeft;
+        public event Action MoveRight;
+    }
+}
---
+++ MyProjectSnake/MySnakeProject/MySnakeProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using MySnakeProject;
+using Shared;
+
+internal class Program
+{
+    public static void Main()
+    {
+        ConsoleInput consoleInput = new ConsoleInput();
+        PlaybleUnitSnake player = new(new Vector2(0, 0), consoleInput);
+
+        ConsoleColor[] colors = { ConsoleColor.Green, ConsoleColor.Blue, ConsoleColor.White };
+        ConsoleRenderer renderer = new ConsoleRenderer(colors);
+
+        while (true)
+        {
+            consoleInput.Update();
+            player.Update(renderer);
+            renderer.Render();
+            renderer.Clear();
+
+
+            Thread.Sleep(100);
+        }
+    }
+}
